dfc696556abbc9588a855feb55685c9def1dae41
GavIn Sokolof <sokolofg@gmail.com>
2019-03-09 03:22:55 -0500

    fixing sector ticker scroll poss replacing css

--- src/components/SectorCard.js
+++ src/components/SectorCard.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
-
+import Numeral from "numeral";
 const SectorCard = props => {
   const { sectorData } = props;
+  let change = sectorData.performance * 100;
+  let pctChange = Numeral(change).format("0.00");
+  let positiveInt = pctChange - pctChange - pctChange;
 
   return (
     <div className="ticker">
-      {sectorData.sector}: {sectorData.performance * 100}
+      <div className="symbol">{sectorData.name}</div>:
+      <div className="priceChange">
+        {pctChange > 0 ? (
+          <p className="positiveChange">%{pctChange}</p>
+        ) : (
+          <p className="negativeChange">%{positiveInt}</p>
+        )}
+      </div>
     </div>
   );
 };
