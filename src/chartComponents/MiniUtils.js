e2110db255aee07349f38242648fc7e69ce2e936
Gavin Sokolof <gavin@thesistesting.com>
2019-08-07 20:42:29 -0400

    added token

--- src/chartComponents/MiniUtils.js
+++ src/chartComponents/MiniUtils.js
@@ -10,7 +10,7 @@ export function setDate(arg) {
 
 export function getData(symbol) {
   const promiseMSFT = fetch(
-    `https://cloud.iexapis.com/v1/stock/${symbol}/chart/6m`
+    `https://cloud.iexapis.com/v1/stock/${symbol}/chart/6m&token=pk_ce5287b13658456797045a3c1a6c1472`
   )
     .then(response => response.json())
     .then(res => setDate(res));
