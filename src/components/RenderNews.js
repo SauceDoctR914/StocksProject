42e32a7b271b2275057623c7139c233f299b1c74
Gavin Sokolof <gavin@thesistesting.com>
2019-11-04 09:46:29 -0500

    first attempt at quick fix for styles

--- src/components/RenderNews.js
+++ src/components/RenderNews.js
@@ -5,27 +5,28 @@ class RenderNews extends Component {
   render() {
     return (
       <a
-        href={this.props.news.news_url}
+        href={this.props.news.url}
         rel="noopener noreferrer"
         target="_blank"
         style={{ textDecoration: "none" }}
       >
         {this.props.news.image_url !== null ? (
           <div className="renderNews">
-            {this.props.news.image_url ? (
-              <div className="imageDiv">
+          
+              <div className="company-image-div">
+              {this.props.news.image ? (
                 <img
                   className="images"
                   alt="News"
-                  src={this.props.news.image_url}
+                  src={this.props.news.image}
                 />
+                ) : null}
               </div>
-            ) : null}
             <div className="headline">
-              <h4>{this.props.news.source_name}</h4>
-              <p>{this.props.news.title}</p>
+              <h4>{this.props.news.source}</h4>
+              <p>{this.props.news.headline}</p>
             </div>
-            <div id="published">{Moment(this.props.news.date).calendar()}</div>
+            <div id="published">{Moment(this.props.news.datetime).calendar()}</div>
           </div>
         ) : null}
       </a>
