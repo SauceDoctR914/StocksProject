f6631dfaaaf0973f181034deee0b31e0c1e21ba6
Gavin Sokolof <gavin@thesistesting.com>
2020-04-18 14:19:20 -0400

    imported fonts from google fixed container on news

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import StockCard from "./StockCard";
 import "../styleSheets/StockContainer.css";
 class StockContainer extends Component {
-  // setInterval(() => this.props.fetchStocks(), 10000000000000000000)
 
   mapData = () => {
     return this.props.stocks.map(stock => {
