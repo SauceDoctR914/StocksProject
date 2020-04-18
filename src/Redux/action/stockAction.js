e390df4ec70f5f7c7d4b3bf08730266d8fc25b55
Gavin Sokolof <gavin@thesistesting.com>
2020-04-18 14:19:07 -0400

    added api key, removed news for excess call

--- src/Redux/action/stockAction.js
+++ src/Redux/action/stockAction.js
@@ -1,13 +1,13 @@
-// const getStocks = stocks => ({ type: "GET_STOCKS", payload: stocks });
-
 const iex_key = process.env.REACT_APP_IEX_KEY
+const iex_key_two = process.env.REACT_APP_IEX_KEYTWOk
 const stockURL =
-  `https://cloud.iexapis.com/v1/stock/market/batch?symbols=GOOG,AMZN,F,SBUX,TSLA,BABA,GPRO,FB,PYPL,ADBE,PFE,VZ,NKE,QCOM,SNE,NVDA,MU,NFLX,ETSY,MDB,PEP,BA,INTC,AAPL,SPOT,MSFT&types=quote,news,company&range=1m&last=1&token=${iex_key}`
+  `https://cloud.iexapis.com/v1/stock/market/batch?symbols=GOOG,AMZN,F,SBUX,TSLA,BABA,GPRO,FB,PYPL,ADBE,PFE,VZ,NKE,QCOM,SNE,NVDA,MU,NFLX,ETSY,MDB,PEP,BA,INTC,AAPL,SPOT,MSFT&types=quote,company&range=1m&last=1&token=${iex_key}`
 
 
 export const fetchStocks = () => {
   return dispatch => {
     return fetch(stockURL)
+      .then(res => console.log(res, "GAVIIIII"))
       .then(res => res.json())
       .then(responseObj => Object.values(responseObj))
       .then(stocks => dispatch({ type: "GET_STOCKS", stocks }))
